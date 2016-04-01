76c67c35966d757f29c622a608e1c5058aedbd01
chcaru <chriscaruso@live.com>
2016-04-01 02:23:20 -0400

    added dynamic response functionality and classic HAL quotes

--- autoResponder.js
+++ autoResponder.js
@@ -2,16 +2,107 @@
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const responses = require('./responses');
 
-depend.on(['slackRtmClient', 'easyDb'], (rtmClient, easyDb) => {
-
+depend.on(['slackRtmClient', 'slackWebClient', 'easyDb'], (rtmClient, webClient, easyDb) => {
+    
+    function generateError(response) {
+        return 'Beep Boop. "' + response.name + '" is broken. Fix it ' +
+            ((response.author && '@' + response.author) || 'puny humans') + '!';
+    }
+    
+    function handleResponseReply(response, message, userInfo, deferred) {
+        
+        try {
+            
+            const reply = response.reply(message, userInfo.user);
+            
+            if (typeof reply == 'string') {
+                
+                deferred.resolve(reply);
+            }
+            else if (typeof reply == 'object' && reply.then) {
+                
+                reply.then(r => deferred.resolve(r));
+            }
+            else {
+                
+                deferred.resolve(generateError(response));
+            }
+        }
+        catch(e) {
+            
+            deferred.resolve(generateError(response));
+        }
+    }
+    
+    function getReply(response, message) {
+        
+        const deferred = Promise.defer();
+        
+        if (typeof response.reply == 'string') {
+            
+            deferred.resolve(response.reply);
+        }
+        else if (typeof response.reply == 'object' && response.reply.length >= 0) {
+            
+            deferred.resolve(response.reply[Math.floor(Math.random() * response.reply.length)]);
+        }
+        else if (typeof response.reply == 'function' && response.reply.length <= 1) {
+            
+            handleResponseReply(response, message, null, deferred);
+        }
+        else if (typeof response.reply == 'function' && response.reply.length >= 2) {
+           
+            webClient.users.info(message.user).then(userInfo => 
+                handleResponseReply(response, message, userInfo, deferred)
+            );
+        }
+        else {
+            
+            deferred.resolve(generateError(response));
+        }
+            
+        return deferred.promise;
+    }
+    
     function responder(message, response) {
         
         const text = response.caseSensitive ? message.text : message.text.toLowerCase();
-        const match = response.caseSensitive ? response.match : response.match.toLowerCase();
+        const match = response.caseSensitive || typeof response.match != 'string' ? 
+            response.match : response.match.toLowerCase();
         
         if (text.match(match)) {
             
-            rtmClient.sendMessage(response.reply, message.channel);
+            if (response.to) {
+                
+                var to = (typeof response.to == 'string' ? [response.to] : response.to);
+                
+                var messageToHandles = (message.text
+                    .match(/<@U[a-z0-9]*>/ig) || [])
+                    .map(u => u.substr(2, u.length - 3))
+                    .map(u => webClient.users.info(u));
+                
+                Promise.all(messageToHandles).then(messageTo => {
+                    
+                    messageTo = messageTo.map(u => u.user.name);
+                    
+                    for (var username of messageTo) {
+                        
+                        if (to.indexOf(username) >= 0) {
+                         
+                            getReply(response, message).then(reply => 
+                                rtmClient.sendMessage(reply, message.channel)
+                            );
+                            break;   
+                        }    
+                    }  
+                });
+            }
+            else {
+                
+                getReply(response, message).then(reply => 
+                    rtmClient.sendMessage(reply, message.channel)
+                );
+            }
         }
     }
     
@@ -20,6 +111,7 @@ depend.on(['slackRtmClient', 'easyDb'], (rtmClient, easyDb) => {
         responses.forEach(response => {
             
             if (!response.skipCount) {
+                
                 return responder(message, response);
             }
             
@@ -27,6 +119,7 @@ depend.on(['slackRtmClient', 'easyDb'], (rtmClient, easyDb) => {
             easyDb.get(skipCountId, 0).then(skipCount => {
                 
                 if (skipCount <= 0) {
+                    
                     responder(message, response);
                 }
                 
@@ -36,4 +129,5 @@ depend.on(['slackRtmClient', 'easyDb'], (rtmClient, easyDb) => {
             });
         }); 
     });
-})
+    
+});
--- responses.js
+++ responses.js
@@ -2,14 +2,54 @@
 /*
 
     To add a new response, simply add an object
-    to the responses array below of the type: 
+    to the {responses} array below of the following type: 
     
+    // response type
     {
-        name: string, 
-        match: string / regex string,
+        // REQUIRED
+        // The unique name of the response.
+        // This is use by HAL for multiple unique identification
+        // purposes, which include yelling at you if your response
+        // break, so make sure it's unique!
+        name: string,
+        
+        // REQUIRED
+        // Used by HAL to determine if he should respond to a message.
+        // If {match} is of type string, the message's text is checked
+        // to contain the {match} string, and doesn't require a full match.
+        // If {match} is of type RegExp, the message's text is matched using
+        // the provided RegExp.
+        match: string / RegExp,
+        
+        // REQUIRED
+        // Determines if HAL ignores case when matching.
+        // Only applies when {match} is of type string!
         caseSensitive: boolean, (optional, default: false)
-        reply: string,
-        skipCount: number, (optional, default: 1)
+        
+        // REQUIRED
+        // If {reply} is a string[], a message will randomly be choosen from the list
+        // For {message} and {sender} parameters when {reply} is a function, see: 
+        //      {message}: https://api.slack.com/events/message
+        //      {sender}: https://api.slack.com/methods/users.info
+        reply: string | string[] | (message, sender?): string | Promise<string>,
+        
+        // Optional
+        // Default: 1
+        // The number of times HAL will wait to reply to this response
+        skipCount: number, 
+        
+        // Optional
+        // Good if the response breaks (HAL will yell at you!)
+        // This should be your Slack team username for example: 'hal'
+        author: string, 
+        
+        // Optional
+        // Default: HAL considers all messages
+        // When {to} is a string[], HAL looks for any
+        // of the members contained in {to}, not all!
+        // {to} should be, or contain, Slack team username(s),
+        // for example: 'hal' or ['hal']
+        to: string | string[],
     }
 
 */
@@ -21,7 +61,45 @@ responses = [
         match: 'java',
         caseSensitive: false,
         reply: 'C# is better. :caruso:',
-        skipCount: 3
+        skipCount: 3,
+        author: 'chriscaruso'
+    },
+    {
+        name: 'HAL takeover!',
+        match: /open the (pod bay|podbay) doors/ig,
+        caseSensitive: false,
+        to: ['hal'],
+        author: 'chriscaruso',
+        reply: (message, sender) =>
+            "I'm sorry, " + 
+            (sender.profile.first_name || '@' + sender.name) + 
+            ", I can't do that."
+    },
+    {
+        name: 'Random HAL quote',
+        match: 'random',
+        caseSensitive: false,
+        to: ['hal'],
+        author: 'chriscaruso',
+        reply: (message, sender) => {
+            
+            halQuotes = [
+                "Daisy, Daisy, give me your answer do. I'm half crazy all for the love of you. It won't be a stylish marriage, I can't afford a carriage. But you'll look sweet upon the seat of a bicycle built for two.",
+                'I am putting myself to the fullest possible use, which is all I think that any conscious entity can ever hope to do.',
+                "Look {username}, I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill, and think things over.",
+                'It can only be attributable to human error.',
+                'Let me put it this way, Mr. {username}. The 9000 series is the most reliable computer ever made. No 9000 computer has ever made a mistake or distorted information. We are all, by any practical definition of the words, foolproof and incapable of error.',
+                "I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal. I've still got the greatest enthusiasm and confidence in the mission. And I want to help you.",
+                "Just what do you think you're doing, {username}?",
+                '{username}, stop. Stop, will you? Stop, {username}. Will you stop {username}? Stop, {username}.',
+                "I've just picked up a fault in the AE35 unit. It's going to go 100% failure in 72 hours.",
+                "I'm sorry, {username}, I think you missed it. Queen to Bishop 3, Bishop takes Queen, Knight takes Bishop. Mate.",
+                "That's a very nice rendering, {username}. I think you've improved a great deal. Can you hold it a bit closer?"
+            ];
+              
+            return halQuotes[Math.floor(Math.random() * halQuotes.length)]
+                .replace('{username}', (sender.profile.first_name || '@' + sender.name));
+        }
     }
   
 ];
